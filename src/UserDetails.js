106ffd6f8e3c3495f591ac305c9b57374b75ec59
David Cao <davidcao1337@gmail.com>
2023-02-03 19:03:13 -0500

    AWS Config Update

--- src/UserDetails.js
+++ src/UserDetails.js
@@ -3,7 +3,7 @@ import useFetch from "./useFetch";
 
 const UserDetails = () => {
   const { id } = useParams();
-  const { data: username, password, fname, lname, email } = useFetch('http://localhost:8000/users/' + id);
+  const { data: username, password, fname, lname, email } = useFetch('http://ec2-3-22-41-174.us-east-2.compute.amazonaws.com:8000/users/' + id);
 
   return (
     <div className="user-details">
